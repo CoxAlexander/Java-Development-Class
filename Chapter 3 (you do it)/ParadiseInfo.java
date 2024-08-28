9735a254f562742584c4932924e68452b336d9d8
CoxAlexander <coxalexander593@gmail.com>
2024-08-28 15:48:21 -0400

    chapter 3

---
+++ Chapter 3 (you do it)/ParadiseInfo.java
@@ -0,0 +1,14 @@
+//ParadiseInfo.Java
+//made by Alexander Cox
+//8/28/2024
+
+
+public class ParadiseInfo{
+    public static void main(String[] args) {
+        displayInfo();
+    }
+    public static void displayInfo() {
+        System.out.println("Paradise Day spa wants to pamper you");
+        System.out.println("We will make you look good");
+    }
+}
