4e77d6ecc9c841e3d4bc15ccc17bcac9b3b07ccb
CoxAlexander <coxalexander593@gmail.com>
2024-09-04 14:54:44 -0400

    chapter 4 start

---
+++ Chapter 4 (you do it)/SpaService.java
@@ -0,0 +1,20 @@
+// SpaService.java
+//9/4/2024
+//Alexander Cox
+
+public class SpaService{
+    private String serviceDescription;
+    private double price;
+    public void setServiceDescription(String service) {
+        serviceDescription=service;
+    }
+    public void setPrice(double servicePrice){
+        price = servicePrice;
+    }
+    public String getServiceDescription(){
+        return serviceDescription;
+    }
+    public double getPrice(){
+        return price;
+    }
+}
