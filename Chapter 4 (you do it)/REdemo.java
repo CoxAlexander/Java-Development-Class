2a01c5ed1b295eb7c07d43b67a1d4126bd6a7439
CoxAlexander <coxalexander593@gmail.com>
2024-09-11 13:24:50 -0400

    chapter 5

---
+++ Chapter 4 (you do it)/REdemo.java
@@ -0,0 +1,12 @@
+//Alexander Cox
+//9/9/2024
+//Application for 4-40 p. 152
+public class REdemo
+{
+ public static void main(String[] args)
+  {
+   RealEstateListing myListing = new RealEstateListing
+    (2, 250000, "1212 Mansion Lane", 6700);
+   myListing.display();
+  }
+ }
